73b695fcb67a7fe90f2c05df9c2afdb6b1f5366f
Oracle Public Cloud User <opc@doguito-vm1.sub03160441320.vcn1.oraclevcn.com>
2025-03-23 02:18:33 +0000

    fix: format

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const productosRouter = require('./routes/productos');
-const productosService = require('./services/productos-service');
+const indexRouter = require("./routes/index");
+const productosRouter = require("./routes/productos");
+const productosService = require("./services/productos-service");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use(cors());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/productos', productosRouter);
+app.use("/", indexRouter);
+app.use("/productos", productosRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -36,19 +36,19 @@ app.use((req, res, next) => {
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 productosService.init().then((productosService) => {
-  app.set('productosService', productosService);
+  app.set("productosService", productosService);
 });
 
-process.on('exit', () => {
-  app.get('productosService').closePool();
+process.on("exit", () => {
+  app.get("productosService").closePool();
 });
 
 module.exports = app;
